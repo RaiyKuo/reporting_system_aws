84ca43e5c91c042bd915917dc1e604b88747f1dd
RaiyKuo <48700379+RaiyKuo@users.noreply.github.com>
2021-03-11 02:07:08 -0600

    build(Global): Fix few bugs about scripts
    BREAKING CHANGE: No

--- scripts/ECR.ps1
+++ scripts/ECR.ps1
@@ -1,5 +1,10 @@
 # This script is use to push all three docker images to AWS ECR repository.
 
+# get-login-password
+(Get-ECRLoginCommand).Password | docker login --username AWS `
+--password-stdin 474850310752.dkr.ecr.us-east-1.amazonaws.com
+
+# Push images to corresponding ECR repository.
 docker tag client_service 474850310752.dkr.ecr.us-east-1.amazonaws.com/client_service:latest
 docker push 474850310752.dkr.ecr.us-east-1.amazonaws.com/client_service:latest
 
--- scripts/build.ps1
+++ scripts/build.ps1
@@ -1,4 +1,5 @@
 # This is a PowerShell script to build all three services into docker images.
+# The docker engine has to be running before starting.
 cd "D:\Coding\Java\reporting_system_aws"
 
 # Copy AWS credentials into Dockerfile folder for building
